28b9bbd8383a8cba335d41d3dd36306dd91cd565
4fab <kvonehr@gmail.com>
2014-04-14 18:44:33 -0400

    help menu cleaned

--- src/Help.java
+++ src/Help.java
@@ -33,23 +33,23 @@ public class Help implements ICommand {
 		System.out.println("Help Menu");
 		System.out.println("**********************************************************");
 		System.out.println("Available Commands: \n\n");
-		System.out.println("	b <Sentence> - Insert sentence before the current line");
-		System.out.println("	i <Sentence> - Insert sentence after the current line");
-		System.out.println("	e <Sentence> - Insert sentence after the last line");
+		System.out.println("	b <Sentence> - Insert sentence before the current line\n");
+		System.out.println("	i <Sentence> - Insert sentence after the current line\n");
+		System.out.println("	e <Sentence> - Insert sentence after the last line\n");
 		System.out.println("	m [#] - Move current line down one line or optional "
-				+ "\nnumber of lines");
+				+ "\n	number of lines\n");
 		System.out.println("	u [#] - Move current line up one line or optional number "
-				+ "\nof lines");
-		System.out.println("	d#* - Display from line # to * (inclusive)");
-		System.out.println("	r - Remove the current line");
-		System.out.println("	c - Clear all lines from file (only if saved)");
+				+ "\n	of lines\n");
+		System.out.println("	d#* - Display from line # to * (inclusive)\n");
+		System.out.println("	r - Remove the current line\n");
+		System.out.println("	c - Clear all lines from file (only if saved)\n");
 		System.out.println("	s <filename> - Save the contents to the specified "
-				+ "\ntext file");
+				+ "\n	text file\n");
 		System.out.println("	l <filename> - Load the contents from the specified text"
-				+ "\n file (only if saved)\n");
+				+ "\n	file (only if saved)\n");
 		System.out.println("	h - Display Help Menu\n");
 		System.out.println("	x - Exit the editor (only if saved)\n\n");
-		System.out.println("**********************************************************");
+		System.out.println("**********************************************************\n");
 		System.out.println(":");
 		
 		// Return that method completed.
