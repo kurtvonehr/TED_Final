8edac930497e81973c9af426e3c43006dd4a4913
JacobMcKim <mckim.jacob@gmail.com>
2014-04-13 20:22:03 -0400

    added command classes

---
+++ src/MoveIndicatorDown.java
@@ -0,0 +1,30 @@
+import java.util.LinkedList;
+
+
+public class MoveIndicatorDown implements ICommand {
+
+	@Override
+	public boolean validCommand(String input) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean executeCommand(String command, LinkedList currentData) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void printError() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void printCommandHelp() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
